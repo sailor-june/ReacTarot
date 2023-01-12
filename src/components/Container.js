63e9c5fcbde3af4644a30f5893ebb76ca76a83b8
Juniper James <vvlpespro@gmail.com>
2023-01-12 08:35:00 -0500

    fixes styling

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Card from './Card'
 function Container({ cards }) {
-  return (
-    cards.map(card => {
+  return (<div className='container'>
+    {cards.map(card => {
         return <Card key={card.name} card={card} />
-    })
-  )
+    })}
+  </div>)
 }
 
 export default Container
